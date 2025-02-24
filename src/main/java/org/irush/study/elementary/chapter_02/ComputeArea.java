2b12f13d63642b3052ddccb9fa52f733fbc9639b
irinaz0407 <irushesta@gmail.com>
2025-02-24 20:30:43 +0100

    new project created: chapter_02

---
+++ src/main/java/org/irush/study/elementary/chapter_02/ComputeArea.java
@@ -0,0 +1,24 @@
+package org.irush.study.elementary.chapter_02;
+
+import java.util.Scanner;
+
+public class ComputeArea {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+        //double radius = 2;
+        double area = areaC(radius);
+
+        // Display results //instead of 'Log' System.out.println
+         System.out.println("The area for the circle of radius " +
+         radius + " is " + area);
+
+    }
+public static double areaC (double radius){
+return radius * radius * 3.1417;
+}
+
+}
